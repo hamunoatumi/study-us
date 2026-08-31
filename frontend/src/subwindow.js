8151a1288f17e3167b2c7cb4b54858bdb514295a
hamunoatumi <syomisora@icloud.com>
2026-08-31 19:21:37 +0900

    chore: Tailwindの適応とPiPで使用できるよう設定

--- frontend/src/subwindow.js
+++ frontend/src/subwindow.js
@@ -3,6 +3,8 @@ let pipWindow = null;
 export async function openSubWindow(participants) {
     pipWindow = await documentPictureInPicture.requestWindow({ width: 400, height: 250});
 
+    copyStylesToPip()       // tailwindcssをpipに適応
+
     pipWindow.document.body.innerHTML = `
     <main class="pip-container">
         <div id="participants"></div>
@@ -25,4 +27,28 @@ function renderParticipants(participants) {
         `
     }).join('')
     sannkasyayouso.innerHTML = participantHtml;
+}
+
+// サブウィンドウのスタイルをコピーする関数
+function copyStylesToPip() {
+  for (const styleSheet of document.styleSheets) {
+    try {
+      const cssText = [...styleSheet.cssRules]
+        .map((rule) => rule.cssText)
+        .join('')
+
+      const style = pipWindow.document.createElement('style')
+      style.textContent = cssText
+
+      pipWindow.document.head.appendChild(style)
+    } catch {
+      if (!styleSheet.href) continue
+
+      const link = pipWindow.document.createElement('link')
+      link.rel = 'stylesheet'
+      link.href = styleSheet.href
+
+      pipWindow.document.head.appendChild(link)
+    }
+  }
 }
