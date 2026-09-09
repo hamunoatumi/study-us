bb3b00f408808322e364b8512df236f6137c12ae
hamunoatumi <syomisora@icloud.com>
2026-09-09 13:06:06 +0900

    fix: 参加ボタンの多重実行を防止

--- frontend/src/home-screen/home.js
+++ frontend/src/home-screen/home.js
@@ -1,14 +1,24 @@
 export function setupHome(onJoin) {
   const usernameInput = document.querySelector('#username')
   const joinButton = document.querySelector('#join-button')
+  let isJoining = false
 
-  joinButton.addEventListener('click', () => {
+  joinButton.addEventListener('click', async () => {
+    if (isJoining) return
     const username = usernameInput.value.trim()
 
     if (!username) {
       console.log("名前を入力してください")
       return
     }
-    onJoin(username)
+    // 参加処理
+    isJoining = true
+
+    try {
+      await onJoin(username)
+    }catch (error) {
+      console.error(error)
+      isJoining = false
+    } 
   })
 }
